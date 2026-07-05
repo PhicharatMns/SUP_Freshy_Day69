78a2d8e29af22c1103e64171f729ec0f868b6c66
Phicharat <phicharatmns@gmail.com>
2026-07-05 18:02:06 +0700

    update env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,16 +7,17 @@ import Qa from './router/Qa.js'
 import ig_my from './router/Ig_my.js'
 import popcar from './router/popcat.js'
 
+
 const app = new Hono()
 
 await connectDB()
 
-
-app.use('*', cors({
-  origin: '*',
+app.use('/*', cors({
+  origin: 'http://147.50.254.93:3000', // URL ของฝั่ง Front-end
   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowHeaders: ["Content-Type", "Authorization"]
 }))
+
+app.options("*", cors());
 
 app.route('/Qafrom', Qa)
 app.route('/ig_my', ig_my)
@@ -24,3 +25,31 @@ app.route('/popcar', popcar)
 
 const port = Number(process.env.PORT) || 5000
 serve({ fetch: app.fetch, port })
+
+
+// import 'dotenv/config'
+// import { serve } from '@hono/node-server'
+// import { Hono } from 'hono'
+// import { connectDB } from './DB/DB.js'
+// import { cors } from 'hono/cors'
+// import Qa from './router/Qa.js'
+// import ig_my from './router/Ig_my.js'
+// import popcar from './router/popcat.js'
+
+// const app = new Hono()
+
+// await connectDB()
+
+
+// app.use('*', cors({
+//   origin: '*',
+//   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowHeaders: ["Content-Type", "Authorization"]
+// }))
+
+// app.route('/Qafrom', Qa)
+// app.route('/ig_my', ig_my)
+// app.route('/popcar', popcar)
+
+// const port = Number(process.env.PORT) || 5000
+// serve({ fetch: app.fetch, port })
