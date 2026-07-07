2cb4fd427b5704feb8e87a5740bf8468dde6a35a
Hoyama1122 <66242350+Hoyama1122@users.noreply.github.com>
2026-07-07 19:39:02 +0700

    setup: update env and add github actions deployment workflow

--- backend/src/DB/DB.ts
+++ backend/src/DB/DB.ts
@@ -1,20 +1,15 @@
-import { Pool } from 'pg'
-import 'dotenv/config'
+import mysql from "mysql2/promise";
+import "dotenv/config";
 
-export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false, // เปลี่ยนตรงนี้เป็น false เพื่อยอมรับ self-signed certificate ของ Supabase
-    }
-})
+export const pool = mysql.createPool(process.env.DATABASE_URL!);
 
 export const connectDB = async () => {
-    try {
-        const client = await pool.connect(); 
-        console.log("🚀 Database connected successfully!");
-        client.release(); 
-    } catch (error) {
-        console.error("❌ Database connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    const conn = await pool.getConnection();
+    console.log("🚀 Database connected successfully!");
+    conn.release();
+  } catch (error) {
+    console.error("❌ Database connection failed:", error);
+    process.exit(1);
+  }
 };
